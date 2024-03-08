f053efafa0923eb41dcba6171deb9e9631e895b8
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-07 19:59:13 -0600

    Se actuañizo validar-roles

--- src/middlewares/validar-roles.js
+++ src/middlewares/validar-roles.js
@@ -1,6 +1,23 @@
-import { Request, response } from "express";
+import { Request, request, response } from "express";
 import User from '../models/User';
 
 export const tieneRole = (...roles) => {
-    
-}
+    return async (req = request, res = response, next) => {
+        if(!req.usuario) {
+            return res.status(500).json({
+                msg:  "No hay un usuario autenticado"
+            });
+        }
+
+
+        if (!roles.includes(req.usuario.role)) {
+            return res.status(401).json({
+                msg: `Este rol no pertenece al siguiente usuario: ${roles}`,
+            });
+        }
+
+        next();
+    };
+};
+
+// Comprueba si el token es correcto y devuelve el usuario
