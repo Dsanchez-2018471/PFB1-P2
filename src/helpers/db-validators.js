6b8a9926f57d2633427b3267fb33b75d31a19013
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-07 18:51:50 -0600

    Actualizacion de helpers

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,23 +1,15 @@
-import Role from '../roles/role.model.js';
-import User from '../Users/user.model.js';
+import Usuario from '../user/user.js';
 
-export const isValiRole = async (role = '') => {
-    const existsRol = await Role.findOne({ role });
-    if (!existsRol) {
-        throw new Error(`The ${role} role does not exist`);
+export const existenteEmail = async (email = '') => {
+    const existeEmail = await Usuario.findOne({email});
+    if(existeEmail){
+        throw new Error(`El email ${ email } ya fue registrado`);
     }
 }
 
-export const existMail = async (mail = '') => {
-    const existMail = await User.findOne({ mail });
-    if (existMail) {
-        throw new Error(`the mail ${mail} has already been registered`)
-    }
-}
-
-export const existUserById = async (id = '') => {
-    const existUser = await User.findById(id);
-    if(!existUser){
-        throw new Error(`The user with id:${id}, doesn't exist in the database`);
+export const existeUsuarioById = async ( id = '') => {
+    const existeUsuario = await Usuario.findOne({id});
+    if(existeUsuario){
+        throw new Error(`El usuario con el id: ${ id } no existe`);
     }
 }
