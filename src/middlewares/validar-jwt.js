17f12bf2eeaff1b255e8efe85cc05768b2f83d44
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-07 19:34:15 -0600

    Se actualizo milddlewares validar-jwt

---
+++ src/middlewares/validar-jwt.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import Usuario from '../Users/user.js';
+
+export const validarJWT = async (req, res, next) => {
+    const token = req.header("x-auth-token");
+    // Verificando si el token está vacío o no existe
+    if (!token) {
+        return res.status(401).json({
+            msg: "No hay un Token proporcionado",
+        });
+    }
+
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        const usuario = await Usuario.findById(uid);
+        if (!usuario) {
+            return res.status(401).json({
+                msg: "El usuario con este token no existe",
+            });
+        }
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: "Este usuario se encuentra inactivo",
+            });
+        }
+        req.usuario = usuario;
+        next();
+    } catch (error) {
+        console.log(error),
+        res.status(401).json({
+            msg: "Token inválido"
+        });
+    }
+};
