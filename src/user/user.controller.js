a3abf4f3ee373a14b76396074b3c45752109bfd0
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-08 20:46:46 -0600

    Se actulizo user y se elimino archivo Users

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,34 +1,77 @@
 import bcryptjs from 'bcryptjs';
 import User from '../user/user.js';
 
-export const userPut = async (req, res) => {
+export const userPutAdmin = async (req, res) => {
     try {
-        const { id }  = req.params;
-        const { nameUser, email, oldPassword, newPassword} = res.body;
+        const { id } = req.params;
+        const { nameUser, email, password, role, estado } = req.body;
 
         const usuarioAutenticado = req.usuario;
-        const idCoincide = usuarioAutenticado._id.toString() === id;
-        const tienePermiso = usuarioAutenticado.rol ==='admin';
+        const tienePermisoAdmin = usuarioAutenticado.role === 'ADMIN_ROLE';
 
-        if (!idCoincide || !tienePermiso ) {
-            return res.status(403).json ({
-                msg: 'No tienes autorizacion para actualizar usuario',
+        if (!tienePermisoAdmin) {
+            return res.status(403).json({
+                msg: 'No estas autorizado para realizar esta accion',
             });
-        }
-
-        if (!oldPassword || !newPassword) {
-            return res.status(400).json({
-                msg: "Debe ingresar la contraseña antigua y nueva"
-            });    
         }
 
         const usuario = await User.findById(id);
-        if(!usuario){
+        if (!usuario) {
             return res.status(400).json({
-                msg: 'Usuario no encontrado'
+                msg: 'Usuario no encontrado',
             });
         }
-        //Verificando de password
+
+        if (nameUser) {
+            usuario.nameUser = nameUser;
+        }
+        if (email) {
+            usuario.email = email;
+        }
+        if (password) {
+            const hashedPassword = await bcryptjs.hash(password, 10); // Nueva contraseña
+            usuario.password = hashedPassword;
+            console.log(`La contraseña se actualizo para el usuario con ID ${id}`);
+        }
+        if (role) {
+            usuario.role = role; 
+        }
+        if (estado !== undefined) { 
+            usuario.estado = estado;
+        }
+        // Guardar los cambios en la base de datos
+        await usuario.save(); 
+
+        res.status(200).json({
+            msg: 'Se realizo la actualizacion correctamente',
+            usuario,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Comunicarse con el administrador',
+        });
+    }
+};
+
+export const userPutClient = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {nameUser, oldPassword, newPassword} = req.body;
+
+        if (!oldPassword || !newPassword) {
+            return res.status(400).json({
+                msg: 'Ingresar la contraseña anterior y la nueva para actualizar',
+            });
+        }
+
+        const usuario = await User.findById(id);
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario no encontrado',
+            });
+        }
+
         const contrasenaValida = await bcryptjs.compare(oldPassword, usuario.password);
         if (!contrasenaValida) {
             return res.status(400).json({
@@ -37,10 +80,7 @@ export const userPut = async (req, res) => {
         }
 
         if (nameUser) {
-            usuario.nameUser= nameUser;
-        }
-        if (email) {
-            usuario.email = email;
+            usuario.nameUser = nameUser;
         }
 
         const hashedPassword = await bcryptjs.hash(newPassword, 10);
@@ -48,15 +88,47 @@ export const userPut = async (req, res) => {
         await usuario.save();
 
         res.status(200).json({
-            msg: 'Se ha actualizado el usuario correctamente',
+            msg: 'Se actualizó el perfil correctamente',
             usuario,
         });
     } catch (error) {
         console.error(error);
         res.status(500).json({
-            msg: 'Comunicarse con el Admin'
+            msg: 'Hable con el administrador',
         });
     }
 };
 
-export default userPut;
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await User.findById(id);
+
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario no existe'
+            });
+        }
+        const usuarioAutenticado = req.usuario;
+        if (usuarioAutenticado.role !== 'ADMIN_ROLE') {
+            return res.status(403).json({
+                msg: 'Unicamente los administradores estan autorizados para eliminar perfiles'
+            });
+        }
+        usuario.estado = false;
+        await usuario.save();
+
+        res.status(200).json({
+            msg: 'Se elimino el perfil',
+            usuario
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Error al eliminar el usuario',
+        });
+    }
+};
+
+
+
