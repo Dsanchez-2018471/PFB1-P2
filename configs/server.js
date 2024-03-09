99d2b3cef3044409a2811cf71dc19ff95f370675
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-03-09 16:00:57 -0600

    server actualizado

--- configs/server.js
+++ configs/server.js
@@ -5,9 +5,10 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js';
-import userRoutes from '../src/Users/user.routes.js';
+import userRoutes from '../src/user/user.routes.js';
 import authRoutes from '../src/auth/auth.routes.js';
-import postRoutes from '../src/post/post.routes.js';
+import categoriasRoutes from '../src/categorias/categoria.routes.js'
+import ProductoRoutes from '../src/productos/producto.routes.js';
 
 class Server{
     constructor(){
@@ -15,7 +16,10 @@ class Server{
         this.port = process.env.PORT;
         this.usuarioPath = '/api/v1/users'
         this.authPath = '/api/v1/auth'
-        this.postPath = '/api/v1/post'
+        this.categoriaPath = '/api/v1/categoria'
+        this.productPath = '/finalAPI/v1/productos';
+    
+
         this.middlewares();
         this.conectarDB();
         this.routes();
@@ -36,12 +40,13 @@ class Server{
     routes(){
         this.app.use(this.usuarioPath, userRoutes);
         this.app.use(this.authPath, authRoutes);
-        this.app.use(this.postPath, postRoutes);
+        this.app.use(this.categoriaPath, categoriasRoutes);
+        this.app.use(this.productPath, ProductoRoutes);
     }
 
     listen(){
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en el puerto', this.port);
+            console.log('El servidor esta en linea', this.port);
         });
     }
 }
